7a64c880f3aef877a48be4dd376cf967d5454138
Yurguen Santos Cruz <yurguen.santos@tangentemexico.com>
2025-09-18 20:32:01 -0600

    interfaz de tabla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CategoryComponent } from './pages/category/category.component';
 
 import { CategoryCreateComponent } from './pages/category/category-create/category-create.component';
 import { CategoryUpdateComponent } from './pages/category/category-update/category-update.component';
+import { CategoryDeleteComponent } from './pages/category/category-delete/category-delete.component';
 
 export const routes: Routes = [
 
@@ -20,6 +21,12 @@ export const routes: Routes = [
     path: 'category/update/:id',
     component: CategoryUpdateComponent
   },
+
+   {
+    path: 'category/delete/:id',
+    component: CategoryDeleteComponent
+  },
+  
 
 
 
