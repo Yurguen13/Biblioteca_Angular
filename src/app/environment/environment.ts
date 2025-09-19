56ccc01236a4a77ec9e18d01f38d04d528abc328
Yoab <kevin02lopez02@gmail.com>
2025-09-18 21:58:43 -0600

    Se agregan los entornos para classifications, specimens y publishers

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -6,5 +6,8 @@ export const environment =
         categories:'http://localhost:2000/api/v1/category',
         author:'http://localhost:2000/api/v1/author',
         books:'http://localhost:2000/api/v1/books',
+         classifications:"http://localhost:2000/api/v1/classifications",
+        specimens:"http://localhost:2000/api/v1/specimens",
+        publishers:"http://localhost:2000/api/v1/publishers"
     }
 }
