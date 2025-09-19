bf466cf5736f4b218de17a746b7116109f8e5220
Yoab <kevin02lopez02@gmail.com>
2025-09-18 19:56:38 -0600

    se agrega la logica de los componentes de classification

--- src/app/pages/classification/classification-delete/classification-delete.component.ts
+++ src/app/pages/classification/classification-delete/classification-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Classification } from '../../../interfaces/classification.module';
+import { ClassificationService } from '../../../services/classification/classification.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-classification-delete',
@@ -7,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrl: './classification-delete.component.css'
 })
 export class ClassificationDeleteComponent {
+  classificationId!:number;
+  classificationData!:Classification;
 
+  constructor(
+    private classificationService: ClassificationService,
+      private route: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ){}
+
+  ngOnInit():void{
+    this.classificationId = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadClassificationData();
+  }
+
+  loadClassificationData(){
+    this.classificationService.getById(this.classificationId).subscribe(data=>{
+      const classification = data;
+      if(classification){
+        this.classificationData=classification;
+      }
+    });
+  }
+
+  deleteClassification(){
+    this.classificationService.deleteClassification(this.classificationId).subscribe({
+      next:()=>{
+        this.snackBar.open('Clasificacion eliminada con exito','Cerrar',{
+          duration:3000,
+          horizontalPosition: 'center',
+            verticalPosition: 'top',
+          panelClass: ['snack-bar-success']
+        });
+      } 
+    });
+  }
 }
--- src/app/pages/classification/classification-list/classification-list.component.ts
+++ src/app/pages/classification/classification-list/classification-list.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Classification } from '../../../interfaces/classification.module';
+import { ClassificationService } from '../../../services/classification/classification.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-classification-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './classification-list.component.html',
   styleUrl: './classification-list.component.css'
 })
 export class ClassificationListComponent {
+  classification:Classification[] = [];
+constructor(private classificationService: ClassificationService){}
 
+ngOnInit():void{
+  this.loadData();
+}
+
+loadData():void{
+  this.classificationService.getClassifications().subscribe(data=>{
+this.classification = data;
+console.log(data);
+  });
+}
 }
